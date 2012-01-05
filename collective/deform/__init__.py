659b98a38791371c5b1254650877659696eafc93
Patrick Gerken <do3cc@patrick-gerken.de>
2012-01-05 13:18:24 +0100

    WIP

--- collective/deform/__init__.py
+++ collective/deform/__init__.py
@@ -1,7 +1,7 @@
 import webob
 import deform
 
-def convert_request(request):
+def convertRequest(request):
     pointer = request.stdin.tell()
     try:
         request.stdin.seek(0)
@@ -9,3 +9,21 @@ def convert_request(request):
         return webob.Request.blank('/', POST=body, environ=request.environ)
     finally:
         request.stdin.seek(pointer)
+
+def translatedForm(schema, buttons, context):
+    def translator(context):
+        def inner_translate(msgid):
+            if hasattr(msgid, 'mapping'):
+                return context.translate(msgid,
+                                         default=msgid.default,
+                                         mapping=msgid.mapping,
+                                         domain=msgid.domain)
+            else:
+                return context.translate(msgid)
+        return inner_translate
+
+    search_path = deform.Form.default_renderer.loader.search_path
+    renderer = deform.template.ZPTRendererFactory(search_path=search_path,
+                                                  translator=translator(context))
+    return deform.Form(schema, buttons=buttons, renderer=renderer)
+
